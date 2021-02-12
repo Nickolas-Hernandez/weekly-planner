48083fd60ae389acc22c6520e28006c94a3baaf2
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-12 12:07:16 -0800

    Nick critically thinking

---
+++ data.js
@@ -0,0 +1,14 @@
+var data = {
+  editing: null,
+  view: 'home-page',
+  entries: {
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: [],
+    sunday: []
+  };
+  nextEntryId: 1
+}
